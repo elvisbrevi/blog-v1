fde885c474939eefd3e1d8d75727dd996834f45e
elvisbrevi <elvisbrevi@gmail.com>
2023-05-11 08:07:34 -0400

    first commit (static website on s3 bucket with cdk)

---
+++ infra/bin/infra.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { FrontendStack } from '../lib/frontend-stack';
+
+const env = { 
+  account: process.env.CDK_DEFAULT_ACCOUNT, 
+  region: 'us-east-1' 
+}; 
+
+const app = new cdk.App();
+new FrontendStack(app, 'FrontendStack', { env: env });
---
+++ infra/lib/frontend-stack.ts
@@ -0,0 +1,74 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3'; 
+import * as route53 from 'aws-cdk-lib/aws-route53'; 
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
+import { Construct } from 'constructs';
+
+const DOMAIN_NAME = "elvisbrevi.com";
+const WWW_DOMAIN_NAME = `www.${DOMAIN_NAME}`;
+
+export class FrontendStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const staticWebsiteBucket = new s3.Bucket(this, `WebsiteBucket-${id}`, {
+      bucketName: DOMAIN_NAME,
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+      accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL
+    });
+
+    const hostedZone = route53.HostedZone.fromLookup(
+      this, `HostedZone`, { domainName: DOMAIN_NAME });
+
+    // Create the HTTPS certificate
+    const httpsCertificate = new acm.Certificate(this, `HttpsCertificate-${id}`, {
+      domainName: DOMAIN_NAME,
+      subjectAlternativeNames: [WWW_DOMAIN_NAME],
+      validation: acm.CertificateValidation.fromDns(hostedZone),
+      certificateName: `Certificate-${id}`,
+    });
+
+    // Create the CloudFront distribution linked to the website hosting bucket and the HTTPS certificate
+    const cloudFrontDistribution = new cloudfront.Distribution(this, `CloudFrontDistribution-${id}`, {
+        defaultBehavior: {
+            origin: new S3Origin(staticWebsiteBucket),
+            viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+        domainNames: [DOMAIN_NAME, WWW_DOMAIN_NAME],
+        certificate: httpsCertificate
+    });
+
+    // Add DNS records to the hosted zone to redirect from the domain name to the CloudFront distribution
+    new route53.ARecord(this, `CloudFrontRedirect-${id}`, {
+      zone: hostedZone,
+      target: route53.RecordTarget.fromAlias(new CloudFrontTarget(cloudFrontDistribution)),
+      recordName: DOMAIN_NAME
+    });
+    
+    // Same from www.sub-domain
+    new route53.ARecord(this, `CloudFrontWWWRedirect-${id}`, {
+        zone: hostedZone,
+        target: route53.RecordTarget.fromAlias(new CloudFrontTarget(cloudFrontDistribution)),
+        recordName: WWW_DOMAIN_NAME
+    });
+
+    // new s3deploy.BucketDeployment(this, `BucketDeployment-${id}`, {
+    //   sources: [s3deploy.Source.asset('../dist')], 
+    //   destinationBucket: staticWebsiteBucket,
+    //   distributionPaths: ['/*'], 
+    //   distribution: cloudFrontDistribution,
+    //   // cacheControl: [
+    //   //     {
+    //   //         value: "max-age=1",
+    //   //     }
+    //   // ]
+    // });
+
+  }
+}
