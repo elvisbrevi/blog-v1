10cedc39dc6be35ce76b0f670c776e05c9a298f3
elvisbrevi <elvisbrevi@gmail.com>
2023-05-23 17:36:49 -0400

    fix name stack in infra and change about page

--- client/src/pages/about/about.tsx
+++ client/src/pages/about/about.tsx
@@ -5,16 +5,17 @@ const AboutPage = () => {
   return (
     <div className="about-container">
       <div className="about-image-container">
-        <h1 className="about-title post-title-font">Hi!</h1>
-        <p className="about-description post-description-font">
+        <h1 className="about-title post-title-font">Contact me!</h1>
+        <SocialLinks />
+        {/* <p className="about-description post-description-font">
           This is my personal 'programmer' website! Here you'll find everything related to my passion for 
           programming. If you're interested in getting to know me better, you can find my official LinkedIn, 
           Twitter, and GitHub accounts. In the near future, I'll also be publishing small projects and 
           sharing my ideas and experiences on my blog. I hope you enjoy my site and if you have any questions, 
           feel free to contact me. Thanks for stopping by!
-        </p>
+        </p> */}
       </div>
-      <SocialLinks />
+      
     </div>
   );
 };
