712e926193f0e7d4d45191cbb2ac7063dd43aefd
elvisbrevi <elvisbrevi@gmail.com>
2023-10-20 08:30:27 -0300

    fixing separator

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -3,19 +3,22 @@ import './header.css';
 const Header = () => {
 
   return (
-    <nav class="navbar navbar-expand-lg">
-      <a class="title" href="/">Elvis Brevi</a>
-      <div class="list navbar-collapse">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="/blog">Blog</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/about">About me</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <>
+      <nav class="navbar navbar-expand-lg">
+        <a class="title" href="/">Elvis Brevi</a>
+        <div class="list navbar-collapse">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="/blog">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about">About me</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <section class="triangle"></section>
+    </>
   );
 };
 
