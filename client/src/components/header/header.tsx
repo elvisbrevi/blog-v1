68cbb60c36028cfeac7197ab78580e21294ca2f9
elvisbrevi <elvisbrevi@gmail.com>
2023-10-20 07:31:15 -0300

    removing unused imports

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -1,20 +1,17 @@
 
 import './header.css';
-import { useEffect, useState } from 'preact/hooks';
-import { Link } from 'react-router-dom';
-
 const Header = () => {
 
   return (
     <nav class="navbar navbar-expand-lg">
-      <a class="title" href="#">Elvis Brevi</a>
+      <a class="title" href="/">Elvis Brevi</a>
       <div class="list navbar-collapse">
         <ul class="navbar-nav">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Blog</a>
+            <a class="nav-link" href="/blog">Blog</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">About me</a>
+            <a class="nav-link" href="/about">About me</a>
           </li>
         </ul>
       </div>
